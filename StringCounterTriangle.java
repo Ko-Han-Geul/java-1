4b9633c23ec04745fb7a2e521f143b51bf78ac9c
Ko-Han-Geul <mongfree@naver.com>
2016-11-02 15:30:00 +0900

    Add files via upload

---
+++ StringCounterTriangle.java
@@ -0,0 +1,29 @@
+class StringCounterTriangle{
+
+void drawTri(){
+
+  String s="*";
+  StringBuffer sbuf=new StringBuffer();
+  String white=" ";
+
+  for (int i=0;i<5;i++){
+    for(int j=5;j>i;j--){
+              sbuf.append(white);
+    }
+    
+    for(int k=0;k<i;k++){
+    sbuf.append(s);
+    }
+  System.out.printf("%s\n" ,sbuf);
+  sbuf.delete(0,sbuf.length());
+  }
+
+}
+
+void main(){
+  StringCounterTriangle t=new StringCounterTriangle();
+  t.drawTri();
+}
+
+
+}
